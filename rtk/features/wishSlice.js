f8c4d743264b186860f7da6532aea6aeb8dbea5b
jiaero <malns7@naver.com>
2022-10-11 18:13:04 +0900

    add .env

--- rtk/features/wishSlice.js
+++ rtk/features/wishSlice.js
@@ -13,9 +13,6 @@ const wishSlice = createSlice({
       state.wish = state.wish.filter((item) => item.id !== action.payload);
     },
   },
-  extraReducers: builder => {
-    builder.removall(PURGE, () => initialState);
-  },
 });
 
 const { actions, reducer } = wishSlice;
