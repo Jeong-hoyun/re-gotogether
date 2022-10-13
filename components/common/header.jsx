2eca58d4eec52c02eb3f0c702d2a1346ac61dc6d
jiaero <malns7@naver.com>
2022-10-13 21:14:57 +0900

    pull

--- components/common/header.jsx
+++ components/common/header.jsx
@@ -1,23 +1,16 @@
 import Link from "next/link";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { useState, useEffect } from "react";
-export default function Header() {
-  const [jwt, setJwt] = useState();
+import { useState } from "react";
+import { useDispatch,useSelector } from 'react-redux';
+import { logout } from "rtk/features/loginSlice";
 
-  useEffect(() => {
-    try {
-      if (localStorage.getItem("jwt")) {
-        setJwt(localStorage.getItem("jwt"));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+export default function Header() {
+  const loginUser = useSelector((state) => state.login.login);
+  const dispatch= useDispatch()
 
   return (
     <>
       <header className="header">
-        <div className=" flex items-center h-20 bg-white border-b border-gray-200 z-10">
+        <div className="z-10 flex items-center h-20 bg-white border-b border-gray-200 ">
           <div className="relative w-full max-w-screen-xl px-6 mx-auto">
             <div className="flex items-center -mx-6">
               <div className="pl-6 pr-6 lg:w-1/4 xl:w-1/5 lg:pr-8">
@@ -41,30 +34,42 @@ export default function Header() {
                     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
                   </svg>
                 </button>
-
-
-                <div className="hidden lg:flex lg:items-center lg:justify-between px-6">
+                <div className="hidden px-6 lg:flex lg:items-center lg:justify-between">
                   <div className="flex justify-start items-center mx-[10vw]">
                     <Link href="/wishlist">
-                      <a className="flex items-center hover:text-gray-700 mr-5">
+                      <a className="flex items-center mr-5 hover:text-gray-700">
                         여행
                       </a>
                     </Link>
-                    {jwt ? (
-                      <button className="flex items-center hover:text-gray-700 mr-5">
+                    {loginUser.username ? (
+                      <button  onClick={()=>dispatch(logout(loginUser.email))}  className="flex items-center mr-5 hover:text-gray-700">
                         로그아웃
                       </button>
                     ) : (
-                      <button className="flex items-center hover:text-gray-700 mr-5">
+                      <Link href="/login">
+                      <a className="flex items-center mr-5 hover:text-gray-700">
                         로그인
-                      </button>
-                    )}
-
-                    <Link href="/">
+                        </a>
+                      </Link>
+                    )}                 
+                    {loginUser.username ? (
+                      <Link href="/mypage">
+                      <a className="flex items-center mr-5 hover:text-gray-700">
+                        마이페이지
+                      </a>
+                    </Link>
+                    ) : (
+                      <Link href="/signup">
                       <a className="flex items-center mr-5 hover:text-gray-700">
                         회원가입
                       </a>
                     </Link>
+                    )}
+
+
+
+
+
                   </div>
                 </div>
               </div>
@@ -75,4 +80,3 @@ export default function Header() {
     </>
   );
 }
- 
--- pages/travel/[productId].jsx
+++ pages/travel/[productId].jsx
@@ -40,7 +40,7 @@ const ProductId = ({ post }) => {
       </Head>
 
       <section className="relative">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="max-w-6xl px-4 mx-auto sm:px-6">
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
             <div>
               <div
