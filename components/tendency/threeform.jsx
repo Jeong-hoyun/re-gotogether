da780a0d5c24ff0de3ec15c75a3fb81ae32db657
Kang-IJ <rkddl6803@naver.com>
2022-10-17 01:42:34 +0900

    feat: add main carousel

--- components/tendency/threeform.jsx
+++ components/tendency/threeform.jsx
@@ -1,4 +1,6 @@
 import { FormWrapper } from "./formwrapper";
+import Image from "next/image";
+
 export function ThreeForm({ three, updateFields }) {
   const onClick = (e) => {
     updateFields({ three: e.target.dataset.img });
@@ -10,7 +12,7 @@ export function ThreeForm({ three, updateFields }) {
         onClick={onClick}
         className="active:shadow-xl text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1 "
       >
-        <img
+        <Image
           src="/img/3_golf.jpg"
           alt="golf"
           data-img="골프"
@@ -23,7 +25,7 @@ export function ThreeForm({ three, updateFields }) {
         onClick={onClick}
         className="active:shadow-xl text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1 "
       >
-        <img
+        <Image
           src="/img/3_historic_site.jpg"
           alt="historic_site"
           data-img="유적지 탐방"
@@ -36,7 +38,7 @@ export function ThreeForm({ three, updateFields }) {
         onClick={onClick}
         className="active:shadow-xl text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1 "
       >
-        <img
+        <Image
           src="/img/3_landscape.jpg"
           alt="landscape"
           data-img="자연 탐방"
@@ -49,7 +51,7 @@ export function ThreeForm({ three, updateFields }) {
         onClick={onClick}
         className="active:shadow-xl text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1 "
       >
-        <img
+        <Image
           src="/img/3_volunteer.jpg"
           alt="volunteer"
           data-img="봉사활동"
@@ -60,7 +62,7 @@ export function ThreeForm({ three, updateFields }) {
       </div>
       <div className="flex flex-wrap w-full mt-5 mb-10 flex-col items-center text-center">
         <p className="text-black-400 text-xl">
-          {three && `${three}를 선택하셨습니다`}
+          {three && `${three}을 선택하셨습니다`}
         </p>
       </div>
     </FormWrapper>
