e97a80c901bb40a121c8bbbb6dde366e417a24fe
Kang-IJ <rkddl6803@naver.com>
2022-10-17 01:41:10 +0900

    style: edit style of forms

--- components/tendency/twoform.jsx
+++ components/tendency/twoform.jsx
@@ -1,4 +1,5 @@
 import { FormWrapper } from "./formwrapper";
+import Image from "next/image";
 
 export function TwoForm({ two, updateFields }) {
   return (
@@ -7,10 +8,10 @@ export function TwoForm({ two, updateFields }) {
         className="active:shadow-xl text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1 "
         onClick={(e) => updateFields({ two: e.target.dataset.img })}
       >
-        <img
+        <Image
           src="/img/2_golf.jpg"
           alt="golf"
-          data-img="golf"
+          data-img="골프"
           className="rounded-lg cursor-pointer hover:opacity-75 transition-all"
           width={300}
           height={200}
@@ -20,7 +21,7 @@ export function TwoForm({ two, updateFields }) {
         className="active:shadow-xl text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1"
         onClick={(e) => updateFields({ two: e.target.dataset.img })}
       >
-        <img
+        <Image
           src="/img/2_hiking.jpg"
           alt="hiking"
           data-img="자연 탐방"
@@ -33,7 +34,7 @@ export function TwoForm({ two, updateFields }) {
         className="active:shadow-xl text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1 "
         onClick={(e) => updateFields({ two: e.target.dataset.img })}
       >
-        <img
+        <Image
           src="/img/2_cultural_site.jpg"
           alt="cultural_site"
           data-img="유적지 탐방"
@@ -46,7 +47,7 @@ export function TwoForm({ two, updateFields }) {
         className="active:shadow-xl text-gray-400 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm py-1 "
         onClick={(e) => updateFields({ two: e.target.dataset.img })}
       >
-        <img
+        <Image
           src="/img/2_volunteer.jpg"
           alt="volunteer"
           data-img="봉사활동"
@@ -55,10 +56,9 @@ export function TwoForm({ two, updateFields }) {
           height={200}
         />
       </div>
-
-      <div className="flex flex-wrap w-full  w-full mt-5 mb-10flex-col items-center text-center">
+      <div className="flex flex-wrap w-full mt-5 mb-10 flex-col items-center text-center">
         <p className="text-black-400 text-xl">
-          {two && `${two}를 선택하셨습니다`}
+          {two && `${two}을 선택하셨습니다`}
         </p>
       </div>
     </FormWrapper>
