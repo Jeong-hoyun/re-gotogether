e0f75d106a8b1ffcfa5f50d3ba3cdfc2dcbfee95
MinJ33 <erica990604@gmail.com>
2022-10-19 00:53:59 +0900

    feat: add responsive header

--- components/common/header.jsx
+++ components/common/header.jsx
@@ -19,59 +19,65 @@ export default function Header() {
       <header className="header">
         <div className="z-10 flex items-center h-20 bg-white border-b border-gray-200 ">
           <div className="relative w-full max-w-screen-xl px-6 mx-auto">
-            <div className="flex items-center -mx-6">
-              <div className="pl-6 pr-6 lg:w-1/4 xl:w-1/5 lg:pr-8">
-                <div className="flex items-center">
+
+            <div id="MainDiv" className="grid grid-cols-2 gap-x-2 grid-flow-row-dense md:flex md:items-center md:mx-6">
+
+              <div id="firstdiv" className="lg:w-1/4 xl:w-1/5 lg:pr-8">
+
+                <div className="shrink-0 block mt-10px lg:mr-4">
                   <Link href="/">
-                    <a className="flex-shrink-0 block lg:mr-4">
-                      <PageLogo/>
+                    <a>
+                      <PageLogo className="w-110px lg:text-xl"/>
                     </a>
                   </Link>
                 </div>
               </div>
+
+
               {/* <Search /> */}
-              {/* 화면이 작아지면 다음줄로 넘어가게 이것저것해보는중 */}
-              <SearchBar className="lg:flex-row"/>
+              {/* 화면이 작아지면 다음줄로 넘어가게*/}
+              <div id="seconddiv" className="col-span-3 mx-auto">
+              <SearchBar/>
+              </div>
 
               <div className="flex flex-wrap items-center justify-end flex-grow">
-                {/* <button className="flex items-center px-6 text-gray-500 lg:hidden focus:outline-none focus:text-gray-700">Í
-                </button> */}
                 <div className="lg:flex lg:items-center ">
-                  <div className="flex ">
+
+                  <div className="flex">
                     <Link href="/wishlist">
-                      <a className="flex items-center mr-5 hover:text-gray-700">
+                      <a className="flex shrink-0 items-center hover:text-gray-700">
                         <Wishlist/>
-                        
-                        <div className="sm:text-xs md:text-sm lg:text-base">관심상품</div> 
+
+                        <div className="mr-2 text-xxs lg:text-base">관심상품</div> 
                       </a>
                     </Link>
                     {loginUser.username ? (
                       <button
                         onClick={() => dispatch(logout(loginUser.email))}
-                        className="flex items-center mr-5 hover:text-gray-700"
+                        className="shrink-0 flex items-center mr-2 hover:text-gray-700"
                       >
                       <Login/>
                         로그아웃
                       </button>
                     ) : (
                       <Link href="/login">
-                        <a className="flex items-center mr-5 hover:text-gray-700">
+                        <a className="flex shrink-0 items-center mr-2 hover:text-gray-700">
                         <Login/>
-                        <div className="sm:text-xs md:text-sm lg:text-base">로그인</div>
+                        <div className="text-xxs lg:text-base">로그인</div>
                         </a>
                       </Link>
                     )}
                     {loginUser.username ? (
                       <Link href="/mypage">
-                        <a className="flex items-center mr-5 hover:text-gray-700">
-                          마이페이지
+                        <a className="flex shrink-0 items-center mr-2 hover:text-gray-700">
+                          <div className="text-xxs lg:text-base">마이페이지</div>
                         </a>
                       </Link>
                     ) : (
                       <Link href="/signup">
-                        <a className="flex items-center mr-5 hover:text-gray-700">
+                        <a className="flex shrink-0 items-center mr-2 hover:text-gray-700">
                           <Signup/>
-                          <div className="sm:text-xs md:text-sm lg:text-base">회원가입</div>
+                          <div className="text-xxs lg:text-base">회원가입</div>
                         </a>
                       </Link>
                     )}
