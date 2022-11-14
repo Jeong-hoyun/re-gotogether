92d028dc23b25b9ed39c8e6edc137235e6537559
jeong-hoyun <ghdbs125@gmail.com>
2022-11-14 19:29:10 +0900

    fix:Add API_URL

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL || "local-build_env"; 
+export const API_URL = process.env.API_URL || "http://ec2-3-36-209-210.ap-northeast-2.compute.amazonaws.com:8080"; 
 
