fdd8ee056d4f130b5450b3e182eb6d6a50b4d535
jeong-hoyun <ghdbs125@gmail.com>
2022-12-15 22:27:14 +0900

    fix:fix amplify.yml

--- apps/web/pages/api/products.ts
+++ apps/web/pages/api/products.ts
@@ -39,6 +39,43 @@ let data = {
       ],
       startDates: ["20220511", "2022061"],
     },
+    {
+      title: "와인 사랑인의 코카서스 3개국 18일",
+      productId: 21,
+      price: 1100000,
+      images: [
+        "https://cdn.imweb.me/thumbnail/20220803/90b39c880066f.png",
+        "https://cdn.imweb.me/thumbnail/20220518/5b276d6a39928.png",
+        "https://cdn.imweb.me/upload/S202107158604372051740/5353fef9d5a4d.jpg",
+        "https://cdn.imweb.me/upload/S202107158604372051740/ea4fec789c8d9.jpg",
+      ],
+      startDates: ["20220511", "2022061"],
+    },
+    {
+      title: "3040들만의 라운드 치앙마이 다색골프",
+      productId: 23,
+      price: 1200000,
+      images: [
+        "https://cdn.imweb.me/thumbnail/20220518/5b276d6a39928.png",
+        "https://cdn.imweb.me/thumbnail/20220518/5b276d6a39928.png",
+        "https://cdn.imweb.me/upload/S202107158604372051740/5353fef9d5a4d.jpg",
+        "https://cdn.imweb.me/upload/S202107158604372051740/ea4fec789c8d9.jpg",
+      ],
+      startDates: ["20220511", "2022061"],
+    },
+    {
+      title: "5070의 버킷리스트 중남미 8개국 34일",
+      productId: 10,
+      price: 1200000,
+      images: [
+        "https://cdn.imweb.me/thumbnail/20220803/9c2e3d5d7ff7e.png",
+        "https://cdn.imweb.me/thumbnail/20220803/9c2e3d5d7ff7e.png",
+        "https://cdn.imweb.me/upload/S202107158604372051740/6904c19f844a0.jpg",
+        "https://cdn.imweb.me/upload/S202107158604372051740/e3995fef15a24.jpg",
+        "https://cdn.imweb.me/upload/S202107158604372051740/b7cbfc2003b1f.jpg",
+      ],
+      startDates: ["20220511", "2022061"],
+    },
   ],
 };
 
